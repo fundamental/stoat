11b0ac959cbf090937b379d4de3ad7fb4659a0a4
fundamental <mark.d.mccurry@gmail.com>
2014-08-24 13:45:41 -0400

    Add Suppression File Feature
    Closes #4

--- test/run-test.rb
+++ test/run-test.rb
@@ -11,11 +11,19 @@ if ARGV.length < 5
     error 1
 end
 
-deductions = ARGV[0]
-llvm_lib   = ARGV[1]
-compiler   = ARGV[2]
-whitelist  = ARGV[3]
-blacklist  = ARGV[4]
+deductions  = ARGV[0]
+llvm_lib    = ARGV[1]
+compiler    = ARGV[2]
+whitelist   = ARGV[3]
+blacklist   = ARGV[4]
+suppression = ARGV[5]
+
+
+if(suppression != "nil")
+    suppression = "-s #{suppression}"
+else
+    suppression = ""
+end
 
 args = ""
 if(/\+\+/.match compiler)
@@ -24,11 +32,20 @@ else
     args = "-std=c11"
 end
 
-source_files = ARGV[5...ARGV.length].join " "
+source_files = ARGV[6...ARGV.length].join " "
 
 `mkdir tmp-build-dir`
 Dir.chdir 'tmp-build-dir'
 `#{compiler} #{args} -emit-llvm -c #{source_files}`
-puts `ruby #{deductions} --llvm-passes #{llvm_lib} -w #{whitelist} -b #{blacklist} -r .`
+puts `ruby #{deductions} --llvm-passes #{llvm_lib} -w #{whitelist} -b #{blacklist} #{suppression} -r .`
+return_code = $?.to_i
 Dir.chdir '..'
 `rm -r tmp-build-dir`
+
+puts "return code = '#{return_code}'"
+
+if(return_code == 0)
+    exit(0)
+else
+    exit(-1)
+end

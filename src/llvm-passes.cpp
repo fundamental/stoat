59be27d240cd8ef91cc2c2bd75a709bbe5bb5f9d
Johannes Lorenz <j.git@lorenz-ho.me>
2019-10-20 08:55:48 +0200

    Add missing header for llvm v9

--- src/llvm-passes.cpp
+++ src/llvm-passes.cpp
@@ -20,6 +20,7 @@
 
 #include <llvm/Analysis/CallGraph.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/Instructions.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Constants.h>
 #include <llvm/IR/LLVMContext.h>
